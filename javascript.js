871baf5307fad3bab1ca4d2177b485e0cbb6a04d
adelp13 <adela.petre13@gmail.com>
2023-05-23 22:23:40 +0300

    add reviews, delete reviews, stop propagation

--- javascript.js
+++ javascript.js
@@ -9,22 +9,65 @@ window.onload = () => {
       changeLikeButton();
   }
 
-function changeLikeButton(){
-      if (!likeCheck){
-        counter++;
-        button.style.background = "rgb(208, 91, 91)";
-        button.style.color = "rgb(0, 255, 153)";
-        button.innerText = 'Dislike';
+  function changeLikeButton(){
+    if (!likeCheck){
+      counter++;
+      button.style.background = "rgb(208, 91, 91)";
+      button.style.color = "rgb(0, 255, 153)";
+      button.innerText = 'Dislike';
+    }
+    else{
+      counter--;
+      button.style.color = "rgb(158, 17, 68)";
+      button.style.background = "#7affca";
+      button.innerHTML = 'Like';
+    }
+    likesCounter.innerHTML = `${counter} LIKES`;
+    likeCheck = 1 - likeCheck;
+  };
+
+  const form = document.getElementById('formular_review');
+
+  const reviewsContainer = document.querySelector('#reviews_container');
+
+  reviewsContainer.addEventListener('click', (e) => {
+      const divList = reviewsContainer.querySelectorAll('div');
+      divList.forEach(element => {
+        element.remove();
+      });
+  });
+
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const new_review = document.createElement('div');
+    
+      let criteriu = form.querySelector('#criteriu').value;
+      let nr_stele = form.querySelector('#nr_stele').value;
+      let continut_review = form.querySelector('#continut_review').value;
+    
+      nr_stele = parseInt(nr_stele);
+      
+      new_review.classList.add('reviews');
+      const h3 = document.createElement('h3');
+      const sp = document.createElement('span');
+      for (let i = 0; i < nr_stele; i++) {
+        sp.innerHTML += '&#9733;';
       }
-      else{
-        counter--;
-        button.style.color = "rgb(158, 17, 68)";
-        button.style.background = "#7affca";
-        button.innerHTML = 'Like';
+      for (let i = 0; i < 10 - nr_stele; i++){
+        sp.innerHTML += '&star;';
       }
-      likesCounter.innerHTML = `${counter} LIKES`;
-      likeCheck = 1 - likeCheck;
-    };
+      p = document.createElement('p');
+      p.textContent = continut_review;
+      sp.style.color = "rgba(226, 183, 183, 0.849)";
+      h3.innerHTML = 'Criteriu selectat: ' + criteriu;
+      new_review.append(h3, sp, p);
+      document.querySelector('#reviews_container').append(new_review);
+
+      new_review.addEventListener('click', (e) => {
+        e.stopPropagation(); 
+        new_review.remove();
+      });
+    });
 }
 
 function generateLikes(){
@@ -38,3 +81,4 @@ function generateLikes(){
         document.getElementById('nr_likes').innerHTML = counter +  ' LIKES';
       }, 7000);
 }
+
